7d079968451b99af71fb10876034b2c5c30c7b30
Ivan LHOMME <i.lhommeageorges@gmail.com>
2025-10-02 14:42:44 +0200

    change the html shortcut button.php to a php file that contain button massage like ' wrong ! ' or ' Good work ... '

---
+++ PHP/buttonMessage.php
@@ -0,0 +1,4 @@
+<?php
+    $rightButtonMessage = "Good work you pass to the next level !";
+    $wrongButtonMessage = ["Nope !", "Try again !", "Wrong button !", "Choose another button !", "No !", "Wrong !"];
+?>
