065f34aeb2e4b9bf8a522a00a344b34c6be08007
Ivan LHOMME <i.lhommeageorges@gmail.com>
2025-10-02 16:55:39 +0200

    add buttonMessage with php and add variable to store indice message on the main page. add style to the page and the text and button to the game.

--- Index.php
+++ Index.php
@@ -1,18 +1,37 @@
+<?php 
+    include "PHP/buttonMessage.php";
+    $indiceMessage = ["Le rouge est jolie non ?", "Que doit-tu trouver dans ce jeu ?"];
+?>
 <!DOCTYPE html>
 <html lang="en">
+
+<style>
+    body{
+        text-align: center;
+    }
+    div{
+        margin-top: 5%;
+    }
+</style>
 
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link href="stylesheet.css" rel="stylesheet"/>
+    <link href="stylesheet_global.css" rel="stylesheet"/>
 
     <title>Find the Button</title>
-    
-    <header><b>FIND THE BUTTON</b></header>
 </head>
 
 <body>
-    <?php echo "hello world !";?>
+    <h1>FIND THE <a id="a1" onclick="alert('<?php echo $rightButtonMessage ?>')">BUTTON !</a></h1>
+
+    <div>
+
+        <h2>Tu dois trouver tout les boutons cachés sur ce site pour gagner !</h2>
+        <p>Pour <a onclick="alert('<?php echo $wrongButtonMessage[random_int(0, 5)] ?>');">commencer</a> le jeu tu dois trouver le <a onclick="alert('<?php echo $wrongButtonMessage[random_int(0, 5)] ?>')">bouton</a>.</p>
+        <p>Deplus tu as le droit à des <a onclick="alert('<?php echo $indiceMessage[random_int(0, 1)] ?>')">indices</a> en haut à droite de la page.</p>
+
+    </div>
 </body>
 
 </html>
