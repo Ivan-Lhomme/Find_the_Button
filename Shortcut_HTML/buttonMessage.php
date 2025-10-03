b3abd54200ed6927374a847791515a67f067027e
Ivan LHOMME <i.lhommeageorges@gmail.com>
2025-10-03 11:56:29 +0200

    add some massage to wrong button click

--- Shortcut_HTML/buttonMessage.php
+++ Shortcut_HTML/buttonMessage.php
@@ -1,4 +1,15 @@
 <?php
     $rightButtonMessage = "Good work you pass to the next level !";
-    $wrongButtonMessage = ["Nope !", "Try again !", "Wrong button !", "Choose another button !", "No !", "Wrong !"];
+    $wrongButtonMessage = [
+        "Nope !", 
+        "Try again !", 
+        "Wrong button !", 
+        "Choose another button !", 
+        "No !", 
+        "Wrong !",
+        "Not this one.",
+        "Keep looking!",
+        "Almost there...",
+        "This isn't it!"
+];
 ?>
