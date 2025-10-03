67a05794df2031f0a3bd8a805cc9581344e8740a
Ivan LHOMME <i.lhommeageorges@gmail.com>
2025-10-03 15:09:19 +0200

    add the title and the hint button and link to the stylesheet page

---
+++ Page/1_FSNfxQhOsdliySInItaz.php
@@ -0,0 +1,35 @@
+<?php
+    $hintMessage = "Test hint message."
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+
+    <style>
+        .hintButton{
+            position: fixed;
+            top: 10%;
+            right: 3%;
+            border-top-right-radius: 0.8em;
+            border-bottom-left-radius: 0.8em;
+            border-bottom-right-radius: 0.5em;
+            border-top-left-radius: 0.5em;
+            background-color: black;
+            color: red;
+            border-color: white;
+            font-size: medium;
+        }
+    </style>
+
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Level 1 - FIND THE BUTTON</title>
+
+    <link href="../stylesheet_global.css" rel="stylesheet"/>
+
+</head>
+<body>
+    <?php include "../Shortcut_HTML/titleAndHintButton.php" ?>
+
+</body>
+</html>
