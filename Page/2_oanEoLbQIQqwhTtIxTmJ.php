8a7d3e4ed3bf599252e6989f432ef7f81a1af8e9
Ivan LHOMME <i.lhommeageorges@gmail.com>
2025-10-05 12:38:53 +0200

    start the level 2

---
+++ Page/2_oanEoLbQIQqwhTtIxTmJ.php
@@ -0,0 +1,15 @@
+<?php
+    include "../Shortcut_HTML/buttonMessage.php";
+    $hintMessage = "hint message";
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <?php include "../Shortcut_HTML/titleAndHintButton.php" ?>
+</body>
+</html>
