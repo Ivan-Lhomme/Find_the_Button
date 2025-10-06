176ab12b2ed31befd8900a1d4f3868b76e6698a1
Ivan LHOMME <i.lhommeageorges@gmail.com>
2025-10-06 09:41:34 +0200

    add some javascript to do some test

--- Page/2_oanEoLbQIQqwhTtIxTmJ.php
+++ Page/2_oanEoLbQIQqwhTtIxTmJ.php
@@ -7,9 +7,21 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>Level 2 - FIND THE BUTTON</title>
+
+    <link href="../stylesheet_global.css" rel="stylesheet"/>
+
 </head>
 <body>
     <?php include "../Shortcut_HTML/titleAndHintButton.php" ?>
+
+    <script>
+        window.addEventListener("load", function () {
+            console.log(
+                "Cette fonction est exécutée une fois quand la page est chargée.",
+            );
+        });
+    </script>
+
 </body>
 </html>
